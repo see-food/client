53ba51de7bf594659f5728a488cd3429787bc075
dmarinsa <dmarinsanchez@gmail.com>
2018-08-02 16:34:23 +0200

    cloudinary

--- src/app/components/photo-upload/photo-upload.component.ts
+++ src/app/components/photo-upload/photo-upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FileSelectDirective, FileUploader } from "ng2-file-upload";
+import { FileUploader } from "ng2-file-upload";
 import { PhotoService } from '../../services/photo.service';
 import { environment } from '../../../environments/environment';
 import { Router } from '@angular/router';
@@ -10,30 +10,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./photo-upload.component.scss']
 })
 export class PhotoUploadComponent implements OnInit {
-  
+
   uploader: FileUploader = new FileUploader({
     url: `${environment.BASE_URL}/api/pics/`,
     method: 'POST'
   });
-
-
-  newPhoto = {
-    username: '',
-    filename: '',
-    url: ''
-  }
+  
   feedback: String;
 
   constructor(private photoService: PhotoService, private router: Router) { }
 
   ngOnInit() {
+    this.uploader.onSuccessItem = (item, response) => {
+      this.feedback = JSON.parse(response).message;
+    };
+
+    this.uploader.onErrorItem = (item, response, status, headers) => {
+      this.feedback = JSON.parse(response).message;
+    }
   }
 
-  submit() {
-    this.uploader.onBuildItemForm = (item, form) => {
-      console.log('forma', form);
-      console.log('item', item)
-    }
+  submit(photo) {
+
     this.uploader.uploadAll();
     console.log(this.uploader);
     this.uploader.onCompleteItem = () => this.router.navigate(['/login']);
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,9 @@ export class PhotoService {
 
 constructor(private http: Http) { }
 
-add(item: any){
-  return this.http.post(`http://localhost:3000/api/pics`, item);
+add(photo: any) {
+  return this.http.post(`${environment.BASE_URL}/api/pics`, photo).pipe(
+    map((res) => res.json())
+  );
 }
 }
