8fe192408dbdb9e067463f9869b8fe2b1cb316b4
jorgeclh <kolmax@gmail.com>
2018-08-06 15:25:18 +0200

    Modify recipes service with new back methods and add photos service

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -12,18 +12,17 @@ import { of } from 'rxjs';
 export class RecipesService {
 
   constructor(private http: Http) { }
+  // newRecipe(recipe) {
+  //   // let { title, category, cuisine, instructions, ingredients } = recipe
+  //   return this.http.post(`${environment.BASE_URL}/api/recipes`, recipe).pipe(
+  //     map((res: Response) => {
+  //       return res.json()
+  //     }),
+  //     catchError(e => of(this.errorHandler(e)))
+  //   )
+  // }
 
-
-  newRecipe(recipe) {
-    // let { title, category, cuisine, instructions, ingredients } = recipe
-    return this.http.post(`${environment.BASE_URL}/api/recipes`, recipe).pipe(
-      map((res: Response) => {
-        return res.json()
-      }),
-      catchError(e => of(this.errorHandler(e)))
-    )
-  }
-
+  //Function that returns all recipes in database
   getRecipes() {
     return this.http.get(`${environment.BASE_URL}/api/recipes`).pipe(
       map((res: Response) => {
@@ -33,6 +32,17 @@ export class RecipesService {
     )
   }
 
+  //Method that returns all of current user starred recipes
+  getRecipesByUser() {
+    return this.http.get(`${environment.BASE_URL}/api/private/recipes`).pipe(
+      map((res: Response) => {
+        return res.json()
+      }),
+      catchError(e => of(this.errorHandler(e)))
+    )
+  }
+
+  //Method that return a single recipe by id
   getRecipeById(id) {
     return this.http.get(`${environment.BASE_URL}/api/recipes/${id}`).pipe(
       map((res: Response) => {
@@ -42,17 +52,17 @@ export class RecipesService {
     )
   }
 
-  getRecipesByUserId() {
-
+  //Method that favs a single recipe for current user
+  starRecipe(id) {
+    return this.http.get(`${environment.BASE_URL}/api/recipes/fav/${id}`).pipe(
+      map((res: Response) => {
+        return res.json()
+      }),
+      catchError(e => of(this.errorHandler(e)))
+    )
   }
 
-
-
-  getRecipesByPhotoId() {
-
-  }
-
-
+  //Error handler method
   errorHandler(e) {
     console.log('RecipesServiceError')
     console.log(e.message);
